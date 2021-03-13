3d86b3181b9a9162f0564d55975fd11eaa2647d3
Divya Gopalakrishnan <deearchitect224@gmail.com>
2021-03-12 21:26:20 -0500

    initial

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../logoshop.png';
+import logo from '../rishablogo.jpg';
 import styled from 'styled-components';
 import { ButtonContainer } from './Button'; 
 
@@ -13,7 +13,7 @@ https://www.iconfinder.com/icons/1243689/call_phone_icon
 Creative Commons (Attribution 3.0 Unported);
 https://www.iconfinder.com/Makoto_msk */}
                 <Link to='/'>
-                    <img src={logo} alt="store" className="navbar-brand" />
+                    <img src={logo} width="48" height="64" alt="store" className="navbar-brand" />
                 </Link>
                 <ul className="navbar-nav align-items-center">
                     <li className="nav-item ml-5">
